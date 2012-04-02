e1d88b5ea9a3d3a3703ce2b9de0eab2d780bb536
Jonah Duckles <jduckles@ou.edu>
2012-04-02 10:28:20 -0500

    Get configurations from mongo rather than local files.

--- eomf/build_html.py
+++ eomf/build_html.py
@@ -2,12 +2,15 @@
 import urllib
 import json
 import pystache
+import sys
+
+taskname = sys.argv[1]
 
 def getdescription(taskname):
-    urlstring = pystache.render("http://test.cybercommons.org/mongo/db_find/taskui/description/%7B'find':%7B'task.taskname':'{{taskname}}'%7D%7D", {'taskname': taskname})
+    urlstring = pystache.render("http://test.cybercommons.org/mongo/db_find/taskui/description/%7B'find':%7B'uiname':'{{taskname}}'%7D%7D", {'taskname': taskname})
     return json.loads(urllib.urlopen(urlstring).read())[0]
 
-description = getdescription('cybercomq.static.tasks.modiscountry');
+description = getdescription(taskname);
 
 print pystache.render(open('tasks.tmpl').read(),description);
 
